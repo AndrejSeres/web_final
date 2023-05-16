4010c67398353636697ab5c230b3b52570ee92aa
ARTIN <andrej.seres@artinsolutions.com>
2023-05-16 15:38:03 +0200

    login/register page

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -27,3 +27,7 @@ Route::post('/register', [RegisterController::class, 'register']);
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
