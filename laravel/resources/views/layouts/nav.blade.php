6e6e666ac6079ea536a5780a1d804a4c97681753
alexanderzichacek <zichacekjunior@gmail.com>
2023-05-17 23:31:18 +0200

    css, lang, blades

--- laravel/resources/views/layouts/nav.blade.php
+++ laravel/resources/views/layouts/nav.blade.php
@@ -1,36 +1,67 @@
-<nav class="navbar navbar-expand-lg blue-header">
-    <div class="container-fluid p-0">
-        
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('menu.toggle') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name', 'WolframAlphaProfessional') }}
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav me-auto">
 
-            </ul>
+                </ul>
 
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('show.home') }}">{{ __('navbar.home') }}</a>
-                    <div class="nav-line"></div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('show.welcome') }}">{{ __('navbar.welcome') }}</a>
-                    <div class="nav-line"></div>
-                </li>
-                <li class="nav-item d-inline-flex align-self-center">
-                    <a class="nav-link pr-1 pl-0 @if(\Lang::locale() == 'sk') font-weight-bolder @endif"
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('show.home') }}">{{ __('navbar.home') }}</a>
+                        <div class="nav-line"></div>
+                    </li>
+                    <li class="nav-item d-inline-flex align-self-center">
+                        <a class="nav-link pr-1 pl-0 @if(\Lang::locale() == 'sk') font-weight-bolder @endif"
                        href="{{ url('locale/sk') }}">{{ __('navbar.lang_sk') }}</a>
-                    <span class="nav-link pr-1 pl-0 pb-1">|</span>
-                    <a class="nav-link pr-1 pl-0 @if(\Lang::locale() == 'en') font-weight-bolder @endif"
+                        <span class="nav-link pr-1 pl-0 pb-1">|</span>
+                        <a class="nav-link pr-1 pl-0 @if(\Lang::locale() == 'en') font-weight-bolder @endif"
                        href="{{ url('locale/en') }}">{{ __('navbar.lang_en') }}</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+                    </li>
+                    <!-- Authentication Links -->
+                    @guest
+                        @if (Route::has('login'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                        @endif
 
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                               data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
+            </div>
+        </div>
+    </nav>
