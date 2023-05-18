7d15a33bdd50160b3b979883059839da122f51ed
alexanderzichacek <zichacekjunior@gmail.com>
2023-05-18 16:46:57 +0200

    update en sk lang for whole site

--- laravel/resources/lang/en/navbar.php
+++ laravel/resources/lang/en/navbar.php
@@ -7,5 +7,8 @@ return [
     'welcome' => 'Welcome page',
     'lang_sk' => 'SK',
     'lang_en' => 'EN',
+    'login' => 'Login',
+    'register' => 'Register',
+    'logout' => 'Logout',
 
 ];
---
+++ laravel/resources/lang/en/register.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'title' => 'Registration',
+    'name' => 'Name',
+    'email' => 'Email address',
+    'password' => 'Password',
+    'password-confirm' => 'Confirm password',
+    'role-student' => 'Student',
+    'role-teacher' => 'Teacher',
+    'register-btn' => 'Register',
+
+];
