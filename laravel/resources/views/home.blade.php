476a157521d495781df6063e8046983cf732febd
Martin Tremko <martin.tremko123@gmail.com>
2023-05-20 18:52:50 +0200

    adding input math expressions into homeStudent

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -1,89 +1,88 @@
 @extends('layouts.app')
 
 @section('content')
-    <main>
-        <section>
-            <div class="container">
-                <div class="row">
-                    <div class="mt-md-5 mb-md-5">
-                        <h3 class="mb-5 mt-5">{{ __('home.welcome') }}</h3>
-                    </div>
+<main>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="mt-md-5 mb-md-5">
+                    <h3 class="mb-5 mt-5">{{ __('home.welcome') }}</h3>
                 </div>
             </div>
+        </div>
 
-        </section>
-    </main>
+    </section>
+</main>
 
-    <div class="container">
-        <div class="row justify-content-center">
-            <div> <!-- Adjusted col-md-10 class -->
-                <div class="card">
-                    <div class="card-header">{{ __('home.dashboard') }}</div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div> <!-- Adjusted col-md-10 class -->
+            <div class="card">
+                <div class="card-header">{{ __('home.dashboard') }}</div>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        {{ __('home.logged-in') }}
-
-                        @auth
-                            <div>
-                                {{ __('home.current-user') }} {{ auth()->user()->name }}
-                            </div>
-
-                            @if (auth()->user()->role === 'teacher')
-                                <div>
-                                    Teacher options
-                                    <button id="show-students-button" class="btn btn-primary">{{ __('home.show-btn') }}</button>
-                                    <div id="table-container" class="mt-3"></div>
-                                </div>
-                            @else
-                                <div>
-                                    <button id="generate-tasks-button" class="btn btn-primary">{{ __('home.generate') }}</button>
-                                </div>
-                                <div id="task-container" class="mt-3"></div>
-                            @endif
-                        @endauth
+                <div class="card-body">
+                    @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
                     </div>
+                    @endif
+
+                    {{ __('home.logged-in') }}
+
+                    @auth
+                    <div>
+                        {{ __('home.current-user') }} {{ auth()->user()->name }}
+                    </div>
+
+                    @if (auth()->user()->role === 'teacher')
+                    <div>
+                        Teacher options
+                        <button id="show-students-button" class="btn btn-primary">{{ __('home.show-btn') }}</button>
+                        <div id="table-container" class="mt-3"></div>
+                    </div>
+                    @else
+                    <div>
+                        <button id="generate-tasks-button" class="btn btn-primary">{{ __('home.generate') }}</button>
+                    </div>
+                    <div id="task-container" class="mt-3"></div>
+                    @endif
+                    @endauth
                 </div>
             </div>
         </div>
     </div>
-
+</div>
 
 <script>
-        // JavaScript code to handle the task generation and display
+    // JavaScript code to handle the task generation and display
 
-        // Get the generate tasks button
-        const generateTasksButton = document.getElementById('generate-tasks-button');
+    // Get the generate tasks button
+    const generateTasksButton = document.getElementById('generate-tasks-button');
 
-        // Get the task container
-        const taskContainer = document.getElementById('task-container');
+    // Get the task container
+    const taskContainer = document.getElementById('task-container');
 
-        // Generate and display tasks when the button is clicked
-        generateTasksButton.addEventListener('click', () => {
-            // Make an AJAX request to fetch the tasks
-            fetch('/generate-tasks')
-                .then(response => response.json())
-                .then(tasks => {
-                    // Clear the task container
-                    taskContainer.innerHTML = '';
+    // Generate and display tasks when the button is clicked
+    generateTasksButton.addEventListener('click', () => {
+        // Make an AJAX request to fetch the tasks
+        fetch('/generate-tasks')
+            .then(response => response.json())
+            .then(tasks => {
+                // Clear the task container
+                taskContainer.innerHTML = '';
 
-                    // Loop through the tasks and create task cards
-                    tasks.forEach(task => {
-                        // Create the task card div
-                        const taskCard = document.createElement('div');
-                        taskCard.classList.add('card', 'mb-3');
+                // Loop through the tasks and create task cards
+                tasks.forEach(task => {
+                    // Create the task card div
+                    const taskCard = document.createElement('div');
+                    taskCard.classList.add('card', 'mb-3');
 
-                        // Create the card body
-                        const cardBody = document.createElement('div');
-                        cardBody.classList.add('card-body');
+                    // Create the card body
+                    const cardBody = document.createElement('div');
+                    cardBody.classList.add('card-body');
 
-                        // Set the card content
-                        cardBody.innerHTML = `
+                    // Set the card content
+                    cardBody.innerHTML = `
                         <h5 class="card-title">${task.name}</h5>
                         <p class="card-text">${task.description}</p>
                         <p class="card-text">${task.formula}</p>
@@ -94,39 +93,55 @@
                         ` : ''}
                         <div class="form-group">
                             <label for="solution_${task.id}">{{ __('home.solution') }}</label>
-                            <input type="text" class="form-control" id="solution_${task.id}">
+                            <span id="math-field" class="math-fieldClass"></span>
                         </div>
                         <div class="form-group">
                             <label>{{ __('home.points') }}${task.points}</label>
                         </div>
                     `;
 
+                    // Append the card body to the task card
+                    taskCard.appendChild(cardBody);
 
-
-
-                        // Append the card body to the task card
-                        taskCard.appendChild(cardBody);
-
-                        // Append the task card to the task container
-                        taskContainer.appendChild(taskCard);
-                    });
-
-                    // Render MathJax to display LaTeX formulas
-                    MathJax.typeset()
-                        .then(() => {
-                            console.log('MathJax typesetting complete.');
-                        })
-                        .catch((err) => {
-                            console.error('Error rendering MathJax:', err);
-                        });
-                })
-                .catch(error => {
-                    console.error('Error fetching tasks:', error);
+                    // Append the task card to the task container
+                    taskContainer.appendChild(taskCard);
                 });
-        });
-    </script>
 
-    <script>
+                var mathFieldSpan = document.getElementById('math-field');
+                console.log(mathFieldSpan);
+                var latexContent;
+
+                var MQ = MathQuill.getInterface(2); // for backcompat
+                var mathField = MQ.MathField(mathFieldSpan, {
+                    spaceBehavesLikeTab: true, // configurable
+                    handlers: {
+                        edit: function() { // useful event handlers
+                            latexContent = mathField.latex(); // simple API
+                            console.log(latexContent);
+                        }
+                    }
+                });
+
+                //OUTPUT Latex file is in the variable latexContent
+                console.log(latexContent);
+
+
+                // Render MathJax to display LaTeX formulas
+                MathJax.typeset()
+                    .then(() => {
+                        console.log('MathJax typesetting complete.');
+                    })
+                    .catch((err) => {
+                        console.error('Error rendering MathJax:', err);
+                    });
+            })
+            .catch(error => {
+                console.error('Error fetching tasks:', error);
+            });
+    });
+</script>
+
+<script>
     // JavaScript to show all students to the teacher
 
     // Function to display the students in the table
