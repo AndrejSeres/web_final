2bbe37d2d6a26f80f4e14b810325994085872148
Andrej Šereš <andrej.seres@artinsolutions.com>
2023-05-22 09:41:06 +0200

    finalize

---
+++ laravel/resources/lang/en/welcome.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'title' => 'Web Application Math-ify',
+    'text' => 'Welcome to the Math-ify website. This page is designed for solving various types of problems for students as well as for teachers who can manage these problems.',
+    'registration' => 'Registration:',
+    'teacher_role' => 'Teacher Role:',
+    'student_role' => 'Student Role:',
+    'teacher_functionalities' => 'Additional Teacher Functionalities:',
+    'teacher_item_1' => 'The teacher has the ability to generate problems from which students can generate examples for solving.',
+    'teacher_item_2' => 'Furthermore, the teacher has the ability to specify the period in which the given problems can be generated.',
+    'teacher_item_3' => 'Additional teacher functionalities:',
+    'teacher_subitem_1' => 'The teacher can define the number of points that can be assigned to a specific student for a certain set of tasks.',
+    'teacher_subitem_2' => 'The teacher has access to a table with statistics containing information about specific students - how many tasks each student generated, how many of them were submitted, and how many points they obtained. The table also shows which tasks each student generated and submitted, the submitted result along with information on whether it was correct, and the number of points obtained for each task.',
+    'student_functionalities' => 'Student Functionalities:',
+    'student_item_1' => 'The student can generate examples for solving.',
+    'student_item_2' => 'The student can also generate an overview of assigned tasks, including the option to submit them. However, each task can be submitted separately.',
+    'student_item_3' => 'Submitting a task involves writing an answer in the form of a mathematical expression.',
+    'download_pdf' => 'Download PDF',
+    'registration_text' => 'Registration differs between the student and teacher roles. During registration, you can choose to be a student or a teacher. The displayed content of the page depends on the chosen role.'
+];
--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -1,62 +1,57 @@
 @extends('layouts.app')
 
 @section('content')
-<main>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.debug.js"></script>
+    <main>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.debug.js"></script>
 
-    <body>
+        <body>
 
         <div class="container alert-info p-3 mt-4" id="textToDownload">
             <div class="row">
                 <div class="col-12">
-                    <h1>Webová aplikácia Math-ify</h1>
+                    <h1>{{ __("welcome.title") }}</h1>
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-12">
-                    <span>Vitajte na stránke Math-ify. Táto stránka slúži na počítanie
-                        rôznych typov príkladov pre študentov, ale aj pre učiteľov,
-                        ktorí môžu dané príklady spravovať.</span>
+                    <span>{{ __("welcome.text") }}</span>
                 </div>
             </div>
 
             <div class="row">
                 <div class="col-md-12">
-                    <h2>Registrácia:</h2>
+                    <h2>{{ __("welcome.registration") }}</h2>
                 </div>
             </div>
 
             <div class="row text-center mb-4">
                 <div class="col-md-12">
-                    Registrácia sa líši od role študenta a učiteľa. Pri registrácií je
-                    možné zvolliť si rolu študent alebo učiteľ. Na základe toho sa odvíja
-                    aj zobrazený obsah stránky.
+                    {{ __("welcome.registration_text") }}
                 </div>
             </div>
 
             <div class="row">
                 <div class="col-md-6">
-                    <h2>Rola učiteľa:</h2>
+                    <h2>{{ __("welcome.teacher_role") }}</h2>
                     <ol>
-                        <li>Učiteľ má možnosť generovania príkladov z ktorých latechových súborov si môžu študenti generovať príklady na riešenie.</li>
-                        <li>Ďalej má možnosť uviesť v ktorom období bude možné dané príkaldy generovať.</li>
-                        <li>Ďalšie funkcionality role učiteľa:</li>
+                        <li>{{ __("welcome.teacher_item_1") }}</li>
+                        <li>{{ __("welcome.teacher_item_2") }}</li>
+                        <li>{{ __("welcome.teacher_functionalities") }}</li>
                         <ol>
-                            <li>Učiteľ môže definovať počet bodov koľko može zapísať konkrétnemu študentovi za určitú sadu úloh.</li>
-                            <li>Má prístup k tabuľke so štatistikou s informáciami o konkrétnych študentoch -
-                                koľko úloh si ktorý študent vygeneroval, koľko ich odovzdal a koľko za ne získal bodov, aké úlohy si ktorý študent vygeneroval, aké odovzdal, odovzdaný výsledok spolu s informáciou, či bol správny a koľko získal za ktorú úlohu bodov.</li>
+                            <li>{{ __("welcome.teacher_subitem_1") }}</li>
+                            <li>{{ __("welcome.teacher_subitem_2") }}</li>
                         </ol>
                     </ol>
                 </div>
                 <div class="col-md-6">
-                    <h2>Rola Študenta:</h2>
+                    <h2>{{ __("welcome.student_role") }}</h2>
                     <ol>
-                        <li>Študent si vie vygenerovať príklady na riešenie.</li>
-                        <li>Zároveň si vie vygenerovať prehľad zadaných úloh, kde sa nachádza aj možnosť odovzdania. Avšak každú úlohu je možné odovzdať samostatne.</li>
-                        <li>Odovzdanie úlohy spočíva v napísaní odpovede, ktorá bude vo forme matematického výrazu.</li>
+                        <li>{{ __("welcome.student_item_1") }}</li>
+                        <li>{{ __("welcome.student_item_2") }}</li>
+                        <li>{{ __("welcome.student_item_3") }}</li>
                     </ol>
                     <div class="col-md-12 text-center pdfButtonDiv">
-                        <button class="btn pdfButton" id="more" onclick="downloadAsPdf()">Stiahnuť pdf</button>
+                        <button class="btn pdfButton" id="more" onclick="downloadAsPdf()">{{ __("welcome.download_pdf") }}</button>
                     </div>
 
                 </div>
@@ -84,6 +79,6 @@
             }
         </script>
 
-    </body>
-</main>
+        </body>
+    </main>
 @endsection
